90c61dd87dba0848e27b78d5ecfc44b5f49cab00
jraitter <jraitter@cableone.net>
2020-01-30 14:48:52 -0700

    finished game over code

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ let healthCount = 100;
 let hitCount = 0;
 let healthElem = document.getElementById("health-count");
 let hitElem = document.getElementById("hit-count");
+let gameOverElem = document.getElementById("game-over");
 
 
 function playGame(hitType) {
-  decrementHealth(hitType);
   hitCount++;
   console.log(hitType);
+  if (healthCount < 1) {
+    healthCount = 0;
+    drawGameOver();
+  } else {
+    decrementHealth(hitType)
+  }
+
   updateGame();
 }
 
@@ -31,6 +38,12 @@ function decrementHealth(item) {
       alert("bad item in switch of decrementHealth()")
       break;
   }
+}
+function drawGameOver() {
+  gameOverElem.innerHTML = (`
+  <div class="card-img-overlay">
+    <h1 class="card-title text-danger">GAME OVER</h1>
+  </div>`);
 }
 
 updateGame();
