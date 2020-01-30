04b360f0291f89f92b11281269a17eb9345fb2c5
jraitter <jraitter@cableone.net>
2020-01-30 15:38:24 -0700

    debugged overlay issue

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 let healthCount = 100;
 let hitCount = 0;
+let playerImage = "./honey-badger.jpg";
+let playerName = "Honey - the badger";
+
+// let player = {
+//   name: "Hone the badger",
+//   image: "./honey-badger.jpg",
+//   hitCount: 0,
+//   healthCount: 100
+// }
+
 let healthElem = document.getElementById("health-count");
 let hitElem = document.getElementById("hit-count");
 let gameOverElem = document.getElementById("game-over");
+let imageElem = document.getElementById("player-image");
+let playerNameElem = document.getElementById("player-name");
 
 
 function playGame(hitType) {
-  hitCount++;
   console.log(hitType);
-  if (healthCount < 1) {
+  decrementHealth(hitType);
+  hitCount++;
+  if (healthCount < 0) {
     healthCount = 0;
-    drawGameOver();
-  } else {
-    decrementHealth(hitType)
+    drawGameOver()
   }
 
   updateGame();
-}
+  // if (healthCount <= 0) {
+  //   healthCount = 0;
+  //   drawGameOver()
+  // } else {
+  //   decrementHealth(hitType)
+  //   hitCount++;
+  // }
 
-function updateGame() {
-  healthElem.innerText = healthCount.toString();
-  hitElem.innerText = hitCount.toString();
 }
 
 function decrementHealth(item) {
@@ -39,6 +53,14 @@ function decrementHealth(item) {
       break;
   }
 }
+
+function updateGame() {
+  healthElem.innerText = healthCount.toString();
+  hitElem.innerText = hitCount.toString();
+  playerNameElem.innerText = playerName;
+  imageElem.src = playerImage;
+}
+
 function drawGameOver() {
   gameOverElem.innerHTML = (`
   <div class="card-img-overlay">
